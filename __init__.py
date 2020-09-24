bc4a06b8438266f101217c1dd8331377fce97bab
Kris Gesling <kris.gesling@mycroft.ai>
2020-09-24 12:17:53 +0930

    show settings on GUI

--- __init__.py
+++ __init__.py
@@ -5,9 +5,13 @@ class TestSkillSettings(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    def initialize(self):
+        self.gui.register_settings()
+
     @intent_handler('display.settings.intent')
     def handle_display_settings(self, message):
         self.speak_dialog('on.screen')
+        self.gui.show_settings()
 
 
 def create_skill():
