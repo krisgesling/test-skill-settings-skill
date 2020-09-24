77359d66fd4443441fbeb4976b077859b5287301
Kris Gesling <kris.gesling@mycroft.ai>
2020-09-24 12:16:33 +0930

    add settings examples

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 
 
 class TestSkillSettings(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('settings.skill.test.intent')
-    def handle_settings_skill_test(self, message):
-        self.speak_dialog('settings.skill.test')
+    @intent_handler('display.settings.intent')
+    def handle_display_settings(self, message):
+        self.speak_dialog('on.screen')
 
 
 def create_skill():
